68989876e990dd6e661474dc32f70fff66dce137
Oded Sharir <oded.sharir@gmail.com>
2026-01-01 20:35:14 +0200

    Enhance ESLint configuration for JavaScript files
    - Add overrides in .eslintrc.js to disable TypeScript-specific rules for JavaScript files, ensuring a smoother linting experience for mixed codebases.

--- .eslintrc.js
+++ .eslintrc.js
@@ -35,6 +35,16 @@ module.exports = {
   },
   overrides: [
     {
+      files: ['**/*.js'],
+      rules: {
+        // Disable TypeScript ESLint rules for JavaScript files
+        '@typescript-eslint/no-var-requires': 'off',
+        '@typescript-eslint/explicit-module-boundary-types': 'off',
+        '@typescript-eslint/no-explicit-any': 'off',
+        '@typescript-eslint/no-unused-vars': 'off',
+      },
+    },
+    {
       files: ['packages/webcomponents/src/**/*.tsx', 'packages/webcomponents/src/**/*.ts'],
       extends: ['plugin:stencil/recommended'],
       rules: {
