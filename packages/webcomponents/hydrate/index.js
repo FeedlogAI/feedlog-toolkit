36ab856bf7dbd27ca689b1200e7f9c5aa0a54d65
Oded Sharir <oded.sharir@gmail.com>
2026-02-28 16:44:15 +0200

    fix: correct node type checks in hydrateFactory function
    - Adjusted conditional checks for slottedNode types to ensure proper handling of comment and text nodes.
    - Refined flag checks for component update conditions to enhance clarity and maintainability.

--- packages/webcomponents/hydrate/index.js
+++ packages/webcomponents/hydrate/index.js
@@ -3121,7 +3121,7 @@ function hydrateFactory(
               !slottedNode['s-sn'] &&
               (!slottedNode['getAttribute'] || !slottedNode.getAttribute('slot')) &&
               (slottedNode.nodeType === 8 /* CommentNode */ ||
-                slottedNode.nodeType === 3) /* TextNode */))
+                slottedNode.nodeType === 3)) /* TextNode */)
         ) {
           slottedNode['s-sn'] = slotName;
           slottedNodes[slotNodeId].push({ slot: slotNode, node: slottedNode, hostId });
@@ -4550,7 +4550,7 @@ function hydrateFactory(
             });
           }
         }
-        if ((flags & (2 /* hasRendered */ | 16) /* isQueuedForUpdate */) === 2 /* hasRendered */) {
+        if ((flags & (2 /* hasRendered */ | 16)) /* isQueuedForUpdate */ === 2 /* hasRendered */) {
           if (instance.componentShouldUpdate) {
             if (instance.componentShouldUpdate(newVal, oldVal, propName) === false) {
               return;
